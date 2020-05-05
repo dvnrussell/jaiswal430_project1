4286c8cf05cf546d761c483aee350d70611a17ad
Devon Russell <dvnrussell@gmail.com>
2020-05-05 17:30:14 -0500

    Renamed some classes/methods to expand

--- src/app/FileFactory.java
+++ src/app/FileFactory.java
@@ -5,9 +5,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class IPFromFileFactory {
+public class FileFactory {
 
-    public static String convertFile(final String filePath) throws IOException {
+    public static String convertAddressFile(final String filePath) throws IOException {
         final BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
         final String addr = br.readLine();
         br.close();
